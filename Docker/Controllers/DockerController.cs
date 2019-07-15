c85c14d5a9a6c5781192b0a9352af9439d335a1e
Евгений Отроков <eotrokov@logrocon.com>
2019-07-15 13:20:59 +0300

    init

---
+++ Docker/Controllers/DockerController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Renci.SshNet;
+
+namespace Docker.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class DockerController : ControllerBase
+    {
+        private string _hostName;
+        private string _login;
+        private string _password;
+        public DockerController()
+        {
+            _hostName = Environment.GetEnvironmentVariable("HOST");
+            _login = Environment.GetEnvironmentVariable("LOGIN");
+            _password = Environment.GetEnvironmentVariable("PASSWORD");
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            string result = "";
+            using (var client = new SshClient(_hostName, _login, _password))
+            {
+                client.Connect();
+                var s = client.RunCommand("docker ps --format \"{{.Names}}\"");
+                result = s.Result;
+                client.Disconnect();
+            }
+            return result.Split(Environment.NewLine).Where(c => !string.IsNullOrEmpty(c)).ToList();
+        }
+
+        [HttpGet("Logs/{name}")]
+        public ActionResult<string> Logs(string name)
+        {
+            string result = "";
+            using (var client = new SshClient(_hostName, _login, _password))
+            {
+                client.Connect();
+                var s = client.RunCommand("docker logs " + name);
+                result = s.Result;
+                client.Disconnect();
+            }
+            return result;
+        }
+        [HttpGet("Start/{name}")]
+        public ActionResult<string> Start(string name)
+        {
+            string result = "";
+            using (var client = new SshClient(_hostName, _login, _password))
+            {
+                client.Connect();
+                var s = client.RunCommand("docker start " + name);
+                result = s.Result;
+                client.Disconnect();
+            }
+            return result;
+        }
+        [HttpGet("Stop/{name}")]
+        public ActionResult<string> Stop(string name)
+        {
+            string result = "";
+            using (var client = new SshClient(_hostName, _login, _password))
+            {
+                client.Connect();
+                var s = client.RunCommand("docker stop " + name);
+                result = s.Result;
+                client.Disconnect();
+            }
+            return result;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# docker_ui" 
+
+docker build -t docker_api .
+docker run -d -p 83:80 --name docker_api --env HOST="host" --env LOGIN="login" --env PASSWORD="password" docker_api
